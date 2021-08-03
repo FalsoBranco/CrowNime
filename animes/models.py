a9962f5004bc3db6f0564885264ffb0e44ea3bf6
helter daniel <helterdaniel@hotmail.com>
2021-08-03 20:10:00 -0300

    WIP modelos base

--- animes/models.py
+++ animes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MediaTitle(models.Model):
+    romanji = models.CharField(max_length=155)
+    english = models.CharField(max_length=155, blank=True, null=True)
+    native = models.CharField(max_length=155, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.romanji
+
+
+class SerieBase(models.Model):
+    title = models.OneToOneField(
+        MediaTitle, related_name="title", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Anime(SerieBase):
+    pass
