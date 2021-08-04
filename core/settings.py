2fc94a2fb1132ff51e3736569461ce6b3e8d5c0d
helter daniel <helterdaniel@hotmail.com>
2021-08-03 21:31:45 -0300

    atualizado .env.Example e settings

--- core/settings.py
+++ core/settings.py
@@ -17,8 +17,8 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
-base = environ.Path(BASE_DIR)
-environ.Env.read_env(env_file=base(".env"))
+
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
